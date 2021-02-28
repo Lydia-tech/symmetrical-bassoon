d78109ed3b09743e5d227c04650841d27343221c
Lydia-tech <71572534+Lydia-tech@users.noreply.github.com>
2021-02-27 19:02:59 -0600

    added code to connect to db

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,9 +1,10 @@
+// create the budget_tracker db on indexedDB
 let db;
 const request = indexedDB.open('budget', 1);
 
 // const db = event.target.result;
 
-db.createObjectStore('pending', { autoIncrement: true });
+db.createObjectStore('new_transaction', { autoIncrement: true });
 
 request.onsuccess = function(event) {
     db = event.target.result;
@@ -17,8 +18,48 @@ request.onerror = function(event) {
 };
 
 function saveRecord(record) {
-    const transaction = db.transaction(['pending'], 'readwrite');
-    const budgetObjectStore = transaction.objectStore('pending');
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+    const budgetObjectStore = transaction.objectStore('new_transaction');
     budgetObjectStore.add(record);
 
 };
+
+function uploadTransaction() {
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+    const transactionObjectStore = transaction.objectStore('new_transaction');
+
+    const getAll = transactionObjectStore.getAll();
+
+    getAll.onsuccess = function() {
+        if(getAll.result.length > 0) {
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(response => response.json())
+            .then(serverResponse => {
+                if (serverResponse.message) {
+                    throw new Error(serverResponse);
+                }
+
+                const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+                const transactionObjectStore = transaction.objectStore('new_transaction');
+
+                transactionObjectStore.clear();
+
+                alert('All saved transactions have been submitted!');
+            })
+            .catch(err => {
+                console.log(err);
+            });
+        }
+    }
+};
+
+window.addEventListener('online', uploadTransaction);
--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -1,20 +1,65 @@
 const APP_PREFIX = 'budget';
 const VERSION = APP_PREFIX + VERSION;
-const CACHE_NAME = APP_PREFIX + VERSION
+const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
-    "./models/transaction.js",
-    "./css/styles.css",
-    "./js/idb.js",
-    "./js/index.js",
-    "./public/index.html",
+
+  './manifest.json',
+  './index.html',
+  './css/style.css',
+  './index/index.js',
+  './js/idb.js',
+  '/',
 ];
 
 self.addEventListener('install', function (e) {
-    e.waitUntil(
-        caches.open(CACHE_NAME).then(function (cache) {
-            console.log('installing cache : ' + CACHE_NAME)
-            return cache.addAll(FILES_TO_CACHE)
-        })
-     )
+	e.waitUntil(
+		caches.open(CACHE_NAME).then(function (cache) {
+			console.log('installing cache : ' + CACHE_NAME);
+			return cache.addAll(FILES_TO_CACHE);
+		})
+	);
+    self.skipWaitings();
 })
+
+self.addEventListener('fetch', function(evt) {
+    if (evt.request.url.includes('/api/')) {
+        evt.respondWith(
+          caches
+            .open(DATA_CACHE_NAME)
+            .then(cache => {
+              return fetch(evt.request)
+                .then(response => {
+                  // 
+                  if (response.status === 200) {
+                    cache.put(evt.request.url, response.clone());
+                  }
+      
+                  return response;
+                })
+                .catch(err => {
+                  // 
+                  return cache.match(evt.request);
+                });
+            })
+            .catch(err => console.log(err))
+        );
+      
+        return;
+      }
+
+      evt.respondWith(
+        fetch(evt.request).catch(function() {
+          return caches.match(evt.request).then(function(response) {
+            if (response) {
+              return response;
+            } else if (evt.request.headers.get('accept').includes('text/html')) {
+              //
+              return caches.match('/');
+            }
+          });
+        })
+      );
+
+
+});
